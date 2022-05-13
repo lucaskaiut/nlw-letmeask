47ff0ddfbb8940fa7a9bf1fb8831a332e9731538
Lucas Kaiut <kaiutdesouza042@gmail.com>
2022-05-13 20:56:43 -0300

    created signin page

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-import "./services/firebase";
+import './services/firebase';
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
+import './styles/global.scss';
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
     <App />
