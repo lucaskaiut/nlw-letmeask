7516924159e2ec6d174c0da44a913ed0f3849de4
Lucas Kaiut <kaiutdesouza042@gmail.com>
2022-05-16 19:43:31 -0300

    created auth context

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,19 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { NewRoom } from './pages/NewRoom';
 
+import { AuthContextProvider } from './contexts/AuthContext';
+
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/rooms/new" element={<NewRoom />} />
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <AuthContextProvider>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/rooms/new" element={<NewRoom />} />
+                </Routes>
+            </BrowserRouter>
+        </AuthContextProvider>
+    );
 }
 
 export default App;
--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -4,28 +4,32 @@ import logoImg from '../assets/images/logo.svg';
 import { Button } from '../components/Button';
 
 import '../styles/auth.scss';
+import { useAuth } from '../hooks/useAuth';
 
 export function NewRoom() {
-  return (
-    <div id="page-auth">
-      <aside>
-        <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
-        <strong>Crie salas de Q&amp;A ao-vivo</strong>
-        <p>Tire as dúvidas da sua audiência em tempo-real</p>
-      </aside>
-      <main>
-        <div className="main-content">
-          <img src={logoImg} alt="Letmeask" />
-          <h2>Criar uma nova sala</h2>
-          <form>
-            <input type="text" placeholder="Nome da Sala" />
-            <Button type="submit">Criar nova sala</Button>
-          </form>
-          <p>
-            Quer entrar em uma sala existente? <Link to="/">clique aqui</Link>
-          </p>
+    const { user } = useAuth();
+
+    return (
+        <div id="page-auth">
+            <aside>
+                <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
+                <strong>Crie salas de Q&amp;A ao-vivo</strong>
+                <p>Tire as dúvidas da sua audiência em tempo-real</p>
+            </aside>
+            <main>
+                <div className="main-content">
+                    <img src={logoImg} alt="Letmeask" />
+                    <h1>{user?.name}</h1>
+                    <h2>Criar uma nova sala</h2>
+                    <form>
+                        <input type="text" placeholder="Nome da Sala" />
+                        <Button type="submit">Criar nova sala</Button>
+                    </form>
+                    <p>
+                        Quer entrar em uma sala existente? <Link to="/">clique aqui</Link>
+                    </p>
+                </div>
+            </main>
         </div>
-      </main>
-    </div>
-  );
+    );
 }
